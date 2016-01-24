e86d83744b5e381a749a80a700bd29db23323cb7
Stijn Vanden Eynde <djcomidi@gmail.com>
2016-01-25 00:32:02 +0100

    completed day 03

---
+++ day03.py
@@ -0,0 +1,41 @@
+with open('day03.txt') as f:
+    data = f.read()
+
+houses = {(0, 0)}
+px, py = 0, 0
+for dir in data:
+    if dir == '^':
+        py -= 1
+    elif dir == 'v':
+        py += 1
+    elif dir == '<':
+        px -= 1
+    elif dir == '>':
+        px += 1
+    houses.add((px, py))
+print("Answer Part 1:", len(houses))
+
+santas, robos = {(0, 0)}, {(0, 0)}
+sx, sy, rx, ry = 0, 0, 0, 0
+for i, dir in enumerate(data):
+    if i % 2 == 0:
+        if dir == '^':
+            sy -= 1
+        elif dir == 'v':
+            sy += 1
+        elif dir == '<':
+            sx -= 1
+        elif dir == '>':
+            sx += 1
+        santas.add((sx, sy))
+    else:
+        if dir == '^':
+            ry -= 1
+        elif dir == 'v':
+            ry += 1
+        elif dir == '<':
+            rx -= 1
+        elif dir == '>':
+            rx += 1
+        robos.add((rx, ry))
+print("Answer Part 2:", len(santas | robos))
