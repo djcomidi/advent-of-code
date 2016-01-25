c952341197160e53fd0647e362b44d2a72d8777f
Stijn Vanden Eynde <djcomidi@gmail.com>
2016-01-25 09:53:53 +0100

    day03: simplifying code

--- day03.py
+++ day03.py
@@ -1,17 +1,21 @@
+def advance(x, y, to):
+    if to == '^':
+        return x, y-1
+    elif to == 'v':
+        return x, y+1
+    elif to == '<':
+        return x-1, y
+    elif to == '>':
+        return x+1, y
+    return 0, 0
+
 with open('day03.txt') as f:
     data = f.read()
 
 houses = {(0, 0)}
 px, py = 0, 0
 for dir in data:
-    if dir == '^':
-        py -= 1
-    elif dir == 'v':
-        py += 1
-    elif dir == '<':
-        px -= 1
-    elif dir == '>':
-        px += 1
+    px, py = advance(px, py, dir)
     houses.add((px, py))
 print("Day 03: Answer Part 1:", len(houses))
 
@@ -19,23 +23,9 @@ santas, robos = {(0, 0)}, {(0, 0)}
 sx, sy, rx, ry = 0, 0, 0, 0
 for i, dir in enumerate(data):
     if i % 2 == 0:
-        if dir == '^':
-            sy -= 1
-        elif dir == 'v':
-            sy += 1
-        elif dir == '<':
-            sx -= 1
-        elif dir == '>':
-            sx += 1
+        sx, sy = advance(sx, sy, dir)
         santas.add((sx, sy))
     else:
-        if dir == '^':
-            ry -= 1
-        elif dir == 'v':
-            ry += 1
-        elif dir == '<':
-            rx -= 1
-        elif dir == '>':
-            rx += 1
+        rx, ry = advance(rx, ry, dir)
         robos.add((rx, ry))
 print("Day 03: Answer Part 2:", len(santas | robos))
